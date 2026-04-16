8017e157cebcf78a40eeb77eb11eecfe56100d51
WorthyOtter <ta.langley05@gmail.com>
2026-04-16 12:15:31 -0700

    Some generic lighting tests. Imported a temporary dungeon tileset. May be permanent if we don't feel like arting.

---
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    public InputAction MoveAction;
+    public InputAction JumpAction;
+
+
+    public Vector2 MoveInput { get; private set; }
+    public bool JumpPressed { get; private set; }
+
+    private void OnEnable()
+    {
+        MoveAction.Enable();
+        JumpAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        MoveAction.Disable();
+        JumpAction.Disable();
+    }
+
+    private void Update()
+    {
+        MoveInput = MoveAction.ReadValue<Vector2>();
+        JumpPressed = JumpAction.triggered;
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("References")]
+    private Rigidbody2D rb;
+    public PlayerInputHandler input;
+
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    public float acceleration = 50f;
+    public float deceleration = 60f;
+
+    [Header("Jump")]
+    public float jumpForce = 10f;
+
+    [Header("Ground Checks")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        if (rb == null) rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        CheckGround();
+
+        if (input.JumpPressed && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        float targetSpeed = input.MoveInput.x * moveSpeed;
+        float speedDiff = targetSpeed - rb.linearVelocityX;
+
+        float accelRate = Mathf.Abs(targetSpeed) > 0.01f ? acceleration : deceleration;
+
+        float movement = speedDiff * accelRate;
+
+        rb.AddForce(Vector2.right * movement);
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocityX, 0f);
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void CheckGround()
+    {
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundCheckRadius,
+            groundLayer
+        );
+    }
+
+    private void OGizmosSelected() //Debug tool
+    {
+        if (groundCheck == null) return;
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+
+}
